47806e2c76e7207aea4b08311069c01f50a87c95
Matthew James Pitsicalis <mjpitsicalis@outlook.com>
2016-04-29 09:39:45 -0400

    Delete ci.sh

--- tests/ci.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo -e "version\nms\nstop\n\n" | php src/pocketmine/PocketMine.php --no-wizard
-if ls plugins/Genisys/Genisys*.phar >/dev/null 2>&1; then
-    echo "Server packaged successfully."
-else
-    echo "No phar created!"
-    exit 1
-fi
