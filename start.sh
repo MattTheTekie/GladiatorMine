a912e8d64aac9924c0e38ed620bf1dcd3a05584f
Matthew James Pitsicalis <mjpitsicalis@outlook.com>
2016-04-30 19:10:18 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -57,7 +57,7 @@ if [ "$POCKETMINE_FILE" == "" ]; then
 	elif [ -f ./src/pocketmine/PocketMine.php ]; then
 		POCKETMINE_FILE="./src/pocketmine/PocketMine.php"
 	else
-		echo "[ERROR] Couldn't find a valid Genisys installation."
+		echo "[ERROR] Couldn't find a valid GladiatorMine installation."
 		exit 1
 	fi
 fi
